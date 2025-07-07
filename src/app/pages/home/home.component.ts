fe1caa16ed41473797e274c4f66933d345ff703e
Juan Lozada <lozadagomezj@gmail.com>
2025-07-06 23:20:12 -0500

    Ajustes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,13 +9,41 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  bannerOpacity = '1';
+    scrollY = 0;
+    width = 0;
 
   @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollY = window.scrollY;
-    const maxScroll = 200; // cuán rápido se desvanece
-    const opacity = Math.max(0, 1 - scrollY / maxScroll);
-    this.bannerOpacity = opacity.toString();
+onScroll() {
+  this.scrollY = window.scrollY;
+  this.width = window.innerWidth;
+}
+
+get logoStyles() {
+  const maxScroll = 300;
+  const ratio = Math.min(this.scrollY / maxScroll, 1);
+  let scale = 1 - 0.6 * ratio; // de 1 a 0.4
+  const translateY = 0 - 130 * ratio; // de 0 a -130px
+  let translateX = -50 - 200 * ratio; // de -50% a -250%
+
+  if (this.width >= 768) {
+    // Pantallas grandes
+    scale = 1 - 0.4 * ratio; // de 1 a 0.6
+    translateX = -50 - 150 * ratio; // de -50% a -200%
+  } else if (this.width >= 480) {
+    // Pantallas medianas
+    translateX = -50 - 70 * ratio; // de -50% a -120%
+  } else  {
+    // Pantallas pequeñas
+    translateX = -50 - 30 * ratio; // de -50% a -120%
   }
+  return {
+    transform: `translate(${translateX}%, ${translateY}px) scale(${scale})`
+  };
+}
+
+get bannerOpacity() {
+  const maxScroll = 300;
+  return 1 - Math.min(this.scrollY / maxScroll, 1);
+}
+
 }
